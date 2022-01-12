3a81ec682c09761b21aabefc946071709f9f6c1e
Opilane TTHK <opilane@tthk.ee>
2022-01-12 16:40:39 +0200

    commit

---
+++ Snake game/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake_game
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+
+        public Point(int _x, int _y, char _symb)
+        {
+            x = _x;
+            y = _y;
+            symb = _symb;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+
+    }
+}
---
+++ Snake game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Snake_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point p1 = new Point(10, 10, '*');            
+            Point p2 = new Point(11, 10, '*');
+
+            HorizontalLine hLine = new HorizontalLine(10, 14, 5, '*');
+            hLine.Draw();
+            VerticalLine vLine = new VerticalLine(6, 16, 10, '@');
+            vLine.Draw();
+
+            //drawing a game field frame
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            top.Draw();
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            left.Draw();
+            HorizontalLine bottom = new HorizontalLine(0, 80, 25, '$');
+            bottom.Draw();
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+            right.Draw();
+
+            Console.ReadLine();
+        }      
+    }
+}
